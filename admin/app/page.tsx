0d397bf634e1ac84cabaf05ff529dd122536cfb6
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-14 23:42:39 +0600

    add a new component

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -3,11 +3,8 @@ import { MousePointer2 } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button className="cursor-pointer" variant={"destructive"} size={"xl"}>
-        Click me
-        <MousePointer2 />
-      </Button>
+    <div className="">
+      
     </div>
   );
 }
