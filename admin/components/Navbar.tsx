bab452e4c9d2aa3f1fd80858f1cceab27f942da4
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-15 10:46:54 +0600

    add Theme to the navbar

--- admin/components/Navbar.tsx
+++ admin/components/Navbar.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-import { LogOut, Moon, Settings, User } from "lucide-react";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import * as React from "react"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,8 +12,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
   return (
     <nav className="flex px-4 items-center justify-between">
       {/*-------Left Side----------*/}
@@ -19,7 +24,28 @@ const Navbar = () => {
       {/*-------Right Side---------*/}
       <div className="flex gap-5 items-center">
         <Link href="/">Dashboard</Link>
-        <Moon />
+        {/*-------------Theme Toggle-------------*/}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/*-------------User Menu-------------*/}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
@@ -30,7 +56,7 @@ const Navbar = () => {
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent sideOffset={5}>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem className="cursor-pointer">
@@ -41,7 +67,7 @@ const Navbar = () => {
               <Settings className="mr-2 h-[1.2rem] w-[1.2rem]" />
               Settings
             </DropdownMenuItem>
-            <DropdownMenuItem className="cursor-pointer">
+            <DropdownMenuItem variant="destructive" className="cursor-pointer">
               <LogOut className="mr-2 h-[1.2rem] w-[1.2rem]" />
               Logout
             </DropdownMenuItem>
