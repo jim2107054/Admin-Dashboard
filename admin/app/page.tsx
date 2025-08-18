98e119b8825a1d70193add8770b572ee41bc98b6
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-18 20:09:14 +0600

    add bar chart page to home page

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,10 +1,11 @@
+import AppBarChart from "@/components/AppBarChart";
 import { Button } from "@/components/ui/button";
 import { MousePointer2 } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 2xl:grid-cols-4">
-      <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+      <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarChart/></div>
       <div className="home-div">Test</div>
       <div className="home-div">Test</div>
       <div className="home-div">Test</div>
