71bbef50ce690fad9ca1f4596b94d657f78fec6b
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-18 19:35:06 +0600

    start creating right side content

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -3,8 +3,13 @@ import { MousePointer2 } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="">
-      
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 2xl:grid-cols-4">
+      <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+      <div className="home-div">Test</div>
+      <div className="home-div">Test</div>
+      <div className="home-div">Test</div>
+      <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
+      <div className="home-div">Test</div>
     </div>
   );
 }
