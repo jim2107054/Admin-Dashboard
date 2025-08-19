18de950921ec4be6eb8faa215fd50a8d4e04926a
jim2107054 <mdjahid205hasan@gmail.com>
2025-08-19 08:42:07 +0600

    add appPieChart component

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,5 +1,6 @@
 import AppAreaChart from "@/components/AppAreaChart";
 import AppBarChart from "@/components/AppBarChart";
+import AppPieChart from "@/components/AppPieChart";
 import { Button } from "@/components/ui/button";
 import { MousePointer2 } from "lucide-react";
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 2xl:grid-cols-4">
       <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarChart/></div>
-      <div className="home-div">Test</div>
+      <div className="home-div"><AppPieChart/></div>
       <div className="home-div">Test</div>
       <div className="home-div">Test</div>
       <div className="home-div lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppAreaChart/></div>
